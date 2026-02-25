a012c34dcdafc4a8a0bb500b5a952024257d2565
Christopher Mazurek <csm012004@outlook.com>
2026-02-25 12:22:59 -0500

    Add project files.

---
+++ CS392MidtermProject/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS392MidtermProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
